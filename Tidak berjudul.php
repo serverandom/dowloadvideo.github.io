1cad16e26bd601cff4e9338e79eaeee794c2925b
serverandom <76838569+serverandom@users.noreply.github.com>
2021-01-01 20:25:01 +0700

    Add files via upload

---
+++ Tidak berjudul.php
@@ -0,0 +1,25 @@
+echo "selamat datang di web download web ini masih awal jadi masih jelek makasih" >> README.md 
+git init 
+git add     <div class="downloader-2 downloader-2-part2"> 
+     <div id="sf_indicator_box" style="display: none;"></div> 
+     <div id="sf_multi_form_box"></div> 
+     <div id="sf_multi_result"></div> 
+     <!--result--> 
+     <div id="sf_result"> 
+      <script>
+              mainFormOutput("Masukkan URL");
+            </script> 
+     </div> 
+     <!--/result--> 
+    </div> 
+    <div id="modal-container" class="modal-container center-popup-cont"> 
+     <div class="shade"></div> 
+     <div class="popup-content"> 
+      <div class="modal-text-cont-for-js"></div> 
+      <div class="close-modal close-modal-icon"></div> 
+     </div> 
+    </div> 
+    <script>README.md 
+git remote add origin https://github.com/serverandom/dowloadvideo. github.io.git
+ git push -u asal main
+                
